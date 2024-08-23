5cb0fc215207d4ecc5c6e2af7b6f3f524ccda154
VadGun <116210360+Def-uIT@users.noreply.github.com>
2024-08-23 18:48:25 +0300

    Autofill values and make button nuts

---
+++ js/main.js
@@ -0,0 +1,22 @@
+let nickname = document.getElementById('nickname');
+let email = document.getElementById('email');
+let password = document.getElementById('password');
+let confirmPassword = document.getElementById('confirmPassword');
+let checkbox = document.getElementById('checkbox');
+const button = document.getElementById('submit');
+
+
+nickname.value = 'Zhora';
+email.value = 'goblin@gmail.com';
+password.value = 'goblin@gmail.com';
+confirmPassword.value = 'goblin@gmail.com';
+checkbox.checked = 'checked'
+
+button.addEventListener('click', () => {
+
+    if (password != confirmPassword) {
+        input.reportValidity();
+    }
+}
+
+)

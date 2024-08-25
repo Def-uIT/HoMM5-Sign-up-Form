7c09d6e86dbb38ae28f4e798852cf937cfcceeb0
VadGun <116210360+Def-uIT@users.noreply.github.com>
2024-08-25 12:52:48 +0300

    Reduce extra elements | Style elements | Add js logic

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
+
 let nickname = document.getElementById('nickname');
 let email = document.getElementById('email');
 let password = document.getElementById('password');
 let confirmPassword = document.getElementById('confirmPassword');
 let checkbox = document.getElementById('checkbox');
+let option = document.getElementById("select");
 const button = document.getElementById('submit');
 
+console.log(option.value);
 
-nickname.value = 'Zhora';
-email.value = 'goblin@gmail.com';
-password.value = 'goblin@gmail.com';
-confirmPassword.value = 'goblin@gmail.com';
-checkbox.checked = 'checked'
+// nickname.value = 'Zhora';
+// email.value = 'goblin@gmail.com';
+// password.value = 'goblin@gmail.com';
+// checkbox.checked = 'checked';
 
-button.addEventListener('click', () => {
-
-}
-
-)
+option.addEventListener('change', () => {
+    const selectedOption = option.value;
+    console.log(selectedOption);
+    if (selectedOption === 'elf') {
+        alert('гей');
+    }
+})
